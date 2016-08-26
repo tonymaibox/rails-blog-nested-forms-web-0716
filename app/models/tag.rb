22fed928f206cde7e8baef9770bcce4a253d730f
Tony Mai <tonymaibox@yahoo.com>
2016-08-26 10:06:53 -0400

    Done.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,6 @@ class Tag < ActiveRecord::Base
   has_many :posts, :through => :post_tags
 
   validates_uniqueness_of :name
+  # validates :name, presence: true
+  # Remember, because we have a uniqueness validation on the name of tag, we will need to account for that; a user shouldn't have to submit a new tag every time they submit a post.
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
 
-  validates_uniqueness_of :name
-  validates_presence_of :name
+  accepts_nested_attributes_for :posts,
+  reject_if: proc {|attributes| attributes['title'].blank?}
+  # validates_uniqueness_of :name
+  # validates_presence_of :name
 end
